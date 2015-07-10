0c327d940e3d718dd971151db56e308b2cfd49c9
Wenjun.Zhang <zhangwenjun1@meizu.com>
2015-07-10 18:27:01 +0800

    First version get CPU/GPU info.

--- cpuinfosave.py
+++ cpuinfosave.py
@@ -1,1 +1,58 @@
 __author__ = 'bigzhang'
+
+from pyadb import ADB
+
+"""
+online_cpu_path=/sys/devices/system/cpu/online
+
+freq_little_path=/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq
+freq_big_path=/sys/devices/system/cpu/cpu4/cpufreq/scaling_cur_freq
+
+load_little_path=/sys/bus/cpu/devices/cpu0/cpufreq/interactive/cpu_util
+load_big_path=/sys/bus/cpu/devices/cpu4/cpufreq/interactive/cpu_util
+"""
+
+online_cpu_path='/sys/devices/system/cpu/online'
+
+freq_little_path='/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq'
+freq_big_path='/sys/devices/system/cpu/cpu4/cpufreq/scaling_cur_freq'
+
+load_little_path='/sys/bus/cpu/devices/cpu0/cpufreq/interactive/cpu_util'
+load_big_path='/sys/bus/cpu/devices/cpu4/cpufreq/interactive/cpu_util'
+
+
+def get_online_cpu(adb):
+    online_cpu_cmd = 'cat ' + online_cpu_path
+    print adb.shell_command(online_cpu_cmd)
+
+def get_frqe_little(adb):
+    freq_little_cmd = 'cat ' + freq_little_path
+    print adb.shell_command(freq_little_cmd)
+
+def get_load_little(adb):
+    freq_little_cmd = 'cat ' + load_little_path
+    print adb.shell_command(freq_little_cmd)
+
+def get_freq_big(adb):
+    freq_big_cmd = 'cat ' + freq_big_path
+    print adb.shell_command(freq_big_cmd)
+
+def get_load_big(adb):
+    load_big_cmd = 'cat ' + load_big_path
+    print adb.shell_command(load_big_cmd)
+
+def main():
+
+    adb = ADB()
+    # set ADB path
+    adb.set_adb_path('/home/bigzhang/Android/Sdk/platform-tools/adb')
+    get_online_cpu(adb)
+
+    get_frqe_little(adb)
+    get_load_little(adb)
+
+    get_freq_big(adb)
+    get_load_big(adb)
+
+if __name__ == "__main__":
+    main()
--- gpuinfosave.py
+++ gpuinfosave.py
@@ -1,1 +1,34 @@
 __author__ = 'bigzhang'
+
+from pyadb import ADB
+
+"""
+# GPU
+power_gpu_path=/sys/bus/platform/devices/14ac0000.mali/power_state
+freq_gpu_path=/sys/bus/platform/devices/14ac0000.mali/clock
+load_gpu_path=/sys/bus/platform/devices/14ac0000.mali/utilization
+"""
+
+power_gpu_path='/sys/bus/platform/devices/14ac0000.mali/power_state'
+freq_gpu_path='/sys/bus/platform/devices/14ac0000.mali/clock'
+load_gpu_path='/sys/bus/platform/devices/14ac0000.mali/utilization'
+
+def get_freq_gpu(adb):
+    freq_gpu_cmd = 'cat ' + freq_gpu_path
+    print adb.shell_command(freq_gpu_cmd)
+
+def get_load_gpu(adb):
+    load_gpu_cmd = 'cat ' + load_gpu_path
+    print adb.shell_command(load_gpu_cmd)
+
+def main():
+
+    adb = ADB()
+    # set ADB path
+    adb.set_adb_path('/home/bigzhang/Android/Sdk/platform-tools/adb')
+
+    get_freq_gpu(adb)
+    get_load_gpu(adb)
+
+if __name__ == "__main__":
+    main()
