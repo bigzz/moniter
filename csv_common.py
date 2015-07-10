b6d0af7b607afdbcc79bf4831fb033751ebd56e6
Wenjun.Zhang <big.zhang@outlook.com>
2015-07-10 23:49:59 +0800

    Add sample cpuinfo

--- csv_common.py
+++ csv_common.py
@@ -2,15 +2,30 @@ __author__ = 'bigzhang'
 
 import csv
 
-def csv_write_cpu(cpuinfo):
-    cpucsv = open('cpuinfo.csv', 'w')
+
+online = [6]
+cpufreq = [800000, 1200000]
+cpuload_l = [100, 100, 100, 0]
+cpuload_b = [100, 100, 100, 0]
+
+cpuinfo = [online, cpufreq, cpuload_l, cpuload_b]
+
+
+def csv_write_cpuinfo(cpuinfo):
+    cpucsv = open('cpuinfo.csv', 'aw+')
     csvfile = csv.writer(cpucsv)
-    csvfile.writerows(cpuinfo)    "write cpuinfo to csv file"
+    csvfile.writerows(cpuinfo)
     cpucsv.close()
 
 
-def csv_write_cpu(cpuinfo, time):
+def csv_read_cpuinfo(cpuinfo, time):
     cpucsv = open('cpuinfo.csv', 'r')
     reader = csv.reader(cpucsv)
 
     cpucsv.close()
+
+def main():
+    csv_write_cpuinfo(cpuinfo)
+
+if __name__ == "__main__":
+    main()
