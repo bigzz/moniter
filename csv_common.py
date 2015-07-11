18fe3acb2d2371e44d36fb9a980c8794cdccefa8
Wenjun.Zhang <big.zhang@outlook.com>
2015-07-11 23:22:52 +0800

    Add thread to handle update the Queue info.

--- csv_common.py
+++ csv_common.py
@@ -2,21 +2,14 @@ __author__ = 'bigzhang'
 
 import csv
 
-def csv_write(func, file, data):
+def csv_write(file, data):
     cpucsv = open(file, 'aw+')
     csvfile = csv.writer(cpucsv)
     csvfile.writerows(data)
     cpucsv.close()
 
-
-def csv_read_cpuinfo(cpuinfo, time):
-    cpucsv = open('cpuinfo.csv', 'r')
+def csv_read_cpuinfo(file, times):
+    cpucsv = open(file, 'r')
     reader = csv.reader(cpucsv)
 
     cpucsv.close()
-
-def main():
-    csv_write_cpuinfo(cpuinfo)
-
-if __name__ == "__main__":
-    main()
--- csv_cpuinfo_write.py
+++ csv_cpuinfo_write.py
@@ -5,6 +5,8 @@ from pyadb import ADB
 import Queue
 import string
 import re
+import time
+import thread
 
 online_cpu_path='/sys/devices/system/cpu/online'
 
@@ -28,9 +30,7 @@ class cpuinfo:
     adb = ADB()
     adb.set_adb_path('/home/bigzhang/Android/Sdk/platform-tools/adb')
 
-    def __init__(self, func_w, func_r, size):
-        self.write = func_w
-        self.read = func_r
+    def __init__(self, size):
         if(size != 10):
             self.cpuinfo = Queue.Queue(size)
 
@@ -40,43 +40,45 @@ class cpuinfo:
         #online = re.findall(r"\d+\.?\d+", tmp)
         online = tmp[2]
         return online
-        #print self.adb.shell_command(online_cpu_cmd)
 
     def get_freq_little(self):
         freq_little_cmd = 'cat ' + freq_little_path
         tmp = self.adb.shell_command(freq_little_cmd)
+        #print tmp
         freq_l = re.findall(r"\d+\.?\d+", tmp)
         return freq_l
-        #print self.adb.shell_command(freq_little_cmd)
 
     def get_load_little(self):
         load_little_cmd = 'cat ' + load_little_path
         tmp = self.adb.shell_command(load_little_cmd)
+        #print tmp
         load_l = re.findall(r"\d+\.?\d+", tmp)
 
         while(len(load_l) < 4):
             load_l.append('0')
 
         return load_l
-        #print self.adb.shell_command(load_little_cmd)
 
     def get_freq_big(self):
         freq_big_cmd = 'cat ' + freq_big_path
-        return self.adb.shell_command(freq_big_cmd)
+        tmp = self.adb.shell_command(freq_big_cmd)
+        #print tmp
         freq_b = re.findall(r"\d+\.?\d+", tmp)
-        return freq_b
-        #print self.adb.shell_command(freq_big_cmd)
+        if freq_b:
+            return freq_b
+        else:
+            return '0'
 
     def get_load_big(self):
         load_big_cmd = 'cat ' + load_big_path
         tmp = self.adb.shell_command(load_big_cmd)
+        #print tmp
         load_b = re.findall(r"\d+\.?\d+", tmp)
 
         while(len(load_b) < 4):
             load_b.append('0')
 
         return load_b
-        #print self.adb.shell_command(load_big_cmd)
 
     def update(self):
         self.online = self.get_online_cpu()
@@ -86,26 +88,46 @@ class cpuinfo:
             self.freq_b = self.get_freq_big()
             self.load_b = self.get_load_big()
         else:
-            self.freq_b = 0
+            self.freq_b = '0'
             self.load_b = ['0', '0', '0', '0']
 
         info = [self.online, self.freq_l, self.load_l, self.freq_b, self.load_b]
 
+        if self.cpuinfo.full():
+            print self.cpuinfo.get()
+
         self.cpuinfo.put(info)
 
-        print self.cpuinfo.get()
-        print self.cpuinfo.get()
+        #print info
+        #print self.cpuinfo.qsize()
 
-    def write(self, func, cpuinfo):
-        csv_common.csv_write(self.func, self.cpufile, self.cpuinfo)
+        self.write(info)
+
+    def write(self, cpui):
+        csv_common.csv_write(self.cpufile, cpui)
 
     def read(self, cpuinfo):
         cpucsv = open(self.cpufile, 'aw+')
 
 
+def timer(cpu, times, interval):
+    cnt = 0
+    while cnt < times:
+        # print 'Thread:(%s) Time:%s/n'%(cnt, time.ctime())
+        time.sleep(interval)
+        cpu.update()
+        cnt += 1
+    thread.exit_thread()
+
+
+def update_thread(cpu):
+    thread.start_new_thread(timer, (cpu, 100, 1))
+
+
 def main():
-    cpui = cpuinfo(0, 0, 20)
-    cpui.update()
+    cpuin = cpuinfo(20)
+    update_thread(cpuin)
+    time.sleep(10000)
 
 
 if __name__ == "__main__":
