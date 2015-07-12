1335befd65fd600fd7e02cd987292e970c6a6aa3
Wenjun.Zhang <big.zhang@outlook.com>
2015-07-12 15:03:59 +0800

    Add CPU Load Show.
    But the graph show not nice.

--- csv_cpuinfo_write.py
+++ csv_cpuinfo_write.py
@@ -1,3 +1,5 @@
+from pyadb.adb import ADB
+
 __author__ = 'bigzhang'
 
 import csv_common
@@ -9,6 +11,7 @@ import time
 import thread
 #import matplotlib.pyplot as plt
 import pylab as pl
+from matplotlib import animation
 
 online_cpu_path='/sys/devices/system/cpu/online'
 
@@ -23,32 +26,40 @@ class cpuinfo:
     cpufile = 'cpuinfo.csv'
 
     online = 0
-    freq_l = 0
-    freq_b = 0
-    load_l = 0
-    load_b = 0
-    cpuinfo = Queue.Queue(maxsize = 10)
+    freq_l = [0]
+    freq_b = [0]
+
+    load_l = []
+    load_l0 = [0] * 20
+    load_l1 = [0] * 20
+    load_l2 = [0] * 20
+    load_l3 = [0] * 20
+
+    load_b = []
+    load_b4 = [0] * 20
+    load_b5 = [0] * 20
+    load_b6 = [0] * 20
+    load_b7 = [0] * 20
 
     adb = ADB()
     adb.set_adb_path('/home/bigzhang/Android/Sdk/platform-tools/adb')
 
-    def __init__(self, size):
-        if(size != 10):
-            self.cpuinfo = Queue.Queue(size)
+    def __init__(self):
+        self.online = 0
 
     def get_online_cpu(self):
         online_cpu_cmd = 'cat ' + online_cpu_path
         tmp = self.adb.shell_command(online_cpu_cmd)
         #online = re.findall(r"\d+\.?\d+", tmp)
         online = tmp[2]
-        return online
+        return string.atoi(online)
 
     def get_freq_little(self):
         freq_little_cmd = 'cat ' + freq_little_path
         tmp = self.adb.shell_command(freq_little_cmd)
         #print tmp
         freq_l = re.findall(r"\d+\.?\d+", tmp)
-        return freq_l
+        return map(int, freq_l)
 
     def get_load_little(self):
         load_little_cmd = 'cat ' + load_little_path
@@ -59,7 +70,7 @@ class cpuinfo:
         while(len(load_l) < 4):
             load_l.append('0')
 
-        return load_l
+        return map(int, load_l)
 
     def get_freq_big(self):
         freq_big_cmd = 'cat ' + freq_big_path
@@ -67,9 +78,9 @@ class cpuinfo:
         #print tmp
         freq_b = re.findall(r"\d+\.?\d+", tmp)
         if freq_b:
-            return freq_b
+            return map(int, freq_b)
         else:
-            return '0'
+            return 0
 
     def get_load_big(self):
         load_big_cmd = 'cat ' + load_big_path
@@ -80,30 +91,65 @@ class cpuinfo:
         while(len(load_b) < 4):
             load_b.append('0')
 
-        return load_b
+        return map(int, load_b)
 
     def update(self):
         self.online = self.get_online_cpu()
-        self.freq_l = self.get_freq_little()
+
+        # Process Little CPU Freq List
+        if len(self.freq_l) >= 20:
+            self.freq_l.pop(0)
+        self.freq_l.append(self.get_freq_little())
+
+        # Process Little CPU Load List
         self.load_l = self.get_load_little()
-        if(string.atoi(self.online) > 3 ):
-            self.freq_b = self.get_freq_big()
+        if len(self.load_l0) >= 20:
+            self.load_l0.pop(0)
+            self.load_l1.pop(0)
+            self.load_l2.pop(0)
+            self.load_l3.pop(0)
+        self.load_l0.append(self.load_l[0])
+        self.load_l1.append(self.load_l[1])
+        self.load_l2.append(self.load_l[2])
+        self.load_l3.append(self.load_l[3])
+
+
+        if(self.online > 3 ):
+            # Process Big CPU Freq List
+            if len(self.freq_b) >= 20:
+                self.freq_b.pop(0)
+            self.freq_b.append(self.get_freq_big())
+
+            # Process B CPU Load List
             self.load_b = self.get_load_big()
+            if len(self.load_b4) >= 20:
+                self.load_b4.pop(0)
+                self.load_b5.pop(0)
+                self.load_b6.pop(0)
+                self.load_b7.pop(0)
+            self.load_b4.append(self.load_b[0])
+            self.load_b5.append(self.load_b[1])
+            self.load_b6.append(self.load_b[2])
+            self.load_b7.append(self.load_b[3])
         else:
-            self.freq_b = '0'
-            self.load_b = ['0', '0', '0', '0']
+            self.freq_b.append(0)
+            self.load_b = [0, 0, 0, 0]
+            if len(self.load_b4) >= 20:
+                self.load_b4.pop(0)
+                self.load_b5.pop(0)
+                self.load_b6.pop(0)
+                self.load_b7.pop(0)
+            self.load_b4.append(self.load_b[0])
+            self.load_b5.append(self.load_b[1])
+            self.load_b6.append(self.load_b[2])
+            self.load_b7.append(self.load_b[3])
 
-        info = [self.online, self.freq_l, self.load_l, self.freq_b, self.load_b]
+        #CPU All Info
+        cinfo = [self.online, self.freq_l, self.load_l, self.freq_b, self.load_b]
 
-        if self.cpuinfo.full():
-            print self.cpuinfo.get()
+        print cinfo
 
-        self.cpuinfo.put(info)
-
-        #print info
-        #print self.cpuinfo.qsize()
-
-        self.write(info)
+        #self.write(cinfo)
 
     def write(self, cpui):
         csv_common.csv_write(self.cpufile, cpui)
@@ -111,8 +157,67 @@ class cpuinfo:
     def read(self, cpuinfo):
         cpucsv = open(self.cpufile, 'aw+')
 
+cpuinfo_g = 0 #save cpuinfo
 
-def timer(cpu, times = 0 , interval = 1):
+fig = pl.figure()
+ax1 = fig.add_subplot(2,1,1,xlim=(0, 20), ylim=(0, 100))
+ax2 = fig.add_subplot(2,1,2,xlim=(0, 20), ylim=(0, 100))
+load_l0, = ax1.plot([], [], lw=2)
+load_l1, = ax1.plot([], [], lw=2)
+load_l2, = ax1.plot([], [], lw=2)
+load_l3, = ax1.plot([], [], lw=2)
+
+load_b4, = ax1.plot([], [], lw=2)
+load_b5, = ax1.plot([], [], lw=2)
+load_b6, = ax1.plot([], [], lw=2)
+load_b7, = ax1.plot([], [], lw=2)
+
+def init():
+    load_l0.set_data([], [])
+    load_l1.set_data([], [])
+    load_l2.set_data([], [])
+    load_l3.set_data([], [])
+    return load_l0, load_l1, load_l2, load_l3
+
+# animation function.  this is called sequentially
+def animate(cpu):
+    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    cpuinfo_g = cpuinfo()
+    load_l0.set_data(x, cpuinfo_g.load_l0)
+    load_l1.set_data(x, cpuinfo_g.load_l1)
+    load_l2.set_data(x, cpuinfo_g.load_l2)
+    load_l3.set_data(x, cpuinfo_g.load_l3)
+
+    load_b4.set_data(x, cpuinfo_g.load_b4)
+    load_b5.set_data(x, cpuinfo_g.load_b5)
+    load_b6.set_data(x, cpuinfo_g.load_b6)
+    load_b7.set_data(x, cpuinfo_g.load_b7)
+    return load_l0, load_l1, load_l2, load_l3, load_b4, load_b5, load_b6, load_b7
+
+def load_show(cpu, interval=1):
+    """
+    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+    pl.plot(x, cpu.load_l0, 'r')
+    pl.plot(x, cpu.load_l1, 'r')
+    pl.plot(x, cpu.load_l2, 'r')
+    pl.plot(x, cpu.load_l3, 'r')
+
+    pl.plot(x, cpu.load_b4, 'g')
+    pl.plot(x, cpu.load_b5, 'g')
+    pl.plot(x, cpu.load_b6, 'g')
+    pl.plot(x, cpu.load_b7, 'g')
+
+    pl.title('CPU Load Info')
+    pl.xlabel('Time')
+    pl.ylabel('Load')
+    pl.xlim(0.0, 20.0)
+    pl.ylim(0.0, 100.0)
+    """
+    anim1 = animation.FuncAnimation(fig, animate, init_func=init, frames=50, interval=0.1)
+    pl.show()
+
+
+def timer(cpu, times=0, interval=1):
     cnt = 0
     if times:
         while cnt < times:
@@ -122,34 +227,22 @@ def timer(cpu, times = 0 , interval = 1):
         thread.exit_thread()
     else:
         while True:
+            time.sleep(interval)
             cpu.update()
         thread.exit_thread()
 
 def update_thread(cpu):
-    thread.start_new_thread(timer, (cpu, 100, 1))
+    thread.start_new_thread(timer, (cpu, 100, 0.1))
+    thread.start_new_thread(load_show, (cpu, 0.1))
 
-
-
-def mulitplot_pic():
-    x1 = [1, 2, 3, 4, 5]
-    y1 = [1, 4, 9, 16, 25]
-    x2 = [1, 2, 4, 6, 8]
-    y2 = [2, 4, 8, 12, 16]
-    pl.plot(x1, y1, 'r')
-    pl.plot(x2, y2, 'g')
-    pl.title('Plot of y vs. x')
-    pl.xlabel('x axis')
-    pl.ylabel('y axis')
-    pl.xlim(0.0, 9.0)
-    pl.ylim(0.0, 30.)
-    pl.show()
 
 def main():
-    cpuin = cpuinfo(20)
+    cpuin = cpuinfo()
+    cpuinfo_g = cpuin
     update_thread(cpuin)
-    mulitplot_pic()
-    time.sleep(10000)
-
+    while True:
+        load_show(cpuin)
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
